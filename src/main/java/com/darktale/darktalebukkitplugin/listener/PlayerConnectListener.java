bf77bde48f730adeef018cf97a1f39248eb656d7
rhin123 <ryanrhin@gmail.com>
2020-01-30 16:24:38 -0600

    Addded APITeleportPlayerEvent

--- src/main/java/com/darktale/darktalebukkitplugin/listener/PlayerConnectListener.java
+++ src/main/java/com/darktale/darktalebukkitplugin/listener/PlayerConnectListener.java
@@ -14,7 +14,7 @@ public class PlayerConnectListener implements Listener {
         //Call the API to tell it that the plugin just recived a player join event
         //The plugin then somehow needs to get told what to do after sending the
 
-        //TODO: The called event should return TRUE/FALSE telling us if we need to cancel the event.
+        //TODO: The called event should return TRUE/FALSE telling us if we need to cancel the event
         DarktaleAPI.getAPI().eventHandler().callEvent(new APIPlayerJoinEvent(event.getPlayer().getUniqueId().toString()));
     }
 
--- src/main/java/com/darktale/darktalebukkitplugin/listener/api/APIPlayerListener.java
+++ src/main/java/com/darktale/darktalebukkitplugin/listener/api/APIPlayerListener.java
@@ -1,10 +1,14 @@
 package com.darktale.darktalebukkitplugin.listener.api;
 
+import com.darktale.darktaleapi.data.world.APILocation;
 import com.darktale.darktaleapi.event.APIEvent;
 import com.darktale.darktaleapi.event.player.APISendPlayerMessageEvent;
+import com.darktale.darktaleapi.event.player.APITeleportPlayerEvent;
 import com.darktale.darktaleapi.listener.APIListener;
 import java.util.UUID;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 /**
  * Listens for API player event calls
@@ -17,4 +21,10 @@ public class APIPlayerListener implements APIListener {
         Bukkit.getPlayer(UUID.fromString(event.getPlayerID())).sendMessage(event.getMessage());
     }
 
+    public void onAPITeleportPlayer(APITeleportPlayerEvent event) {
+        APILocation l = event.getLocation();
+        Player player = Bukkit.getPlayer(UUID.fromString(event.getPlayerID()));
+
+        player.teleport(new Location(player.getWorld(), l.getX(), l.getY(), l.getZ()));
+    }
 }
